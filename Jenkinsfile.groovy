aefd94375215ee20aad6f3b576e8d319a94ffa48
Dicky Wong <dickywmc2004@yahoo.com.hk>
2023-07-14 16:56:49 -0700

    check

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,7 +32,7 @@ pipeline {
         stage('Compile') {
             //echo 'currentBuild.result'
             when {
-                expression {currentBuild.result != 'SUCCESS'}
+                expression {currentBuild.currentResult != 'SUCCESS'}
             }
             steps {
                 sh 'zowe zos-jobs submit data-set "Z90319.JCL(COMPILE)" --wfo --rff retcode --rft string --reject-unauthorized false'
