72868c8bf5ff1d205c0cd459de8ea7b2e8231468
Dicky Wong <dickywmc2004@yahoo.com.hk>
2023-07-19 21:12:02 -0700

    SYST

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -47,7 +47,7 @@ pipeline {
                     //sh 'pwd'
                     //sh 'ls'
                     //git branch: 'main', url: 'https://github.com/dickywmc/DevOps.git'
-                    git branch: 'main', url: url
+                    git branch: branch, url: url
                 }
                 // Upload COBOL program to mainframe (can be parameterized)
                 //sh 'zowe zos-files upload file-to-data-set "CBL0001.cbl" "Z90319.CBL(CBL0001)" --reject-unauthorized false'
